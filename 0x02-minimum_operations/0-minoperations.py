45cb21f52fb52e4454be762d5ac94a4272cbf92d
mainangaruiya <003paulmaina@gmail.com>
2023-10-11 10:14:45 +0300

    In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
    Prototype: def minOperations(n)
    Returns an integer
    If n is impossible to achieve, return 0
    Example:
    
    n = 9
    
    H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
    
    Number of operations: 6

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,22 +4,19 @@ min op file
 """
 
 def minOperations(n):
+    """return n if impossible or 0"""
     if n <= 1:
         return 0
 
-    operations = [0] * (n + 1)
-
+        dp = [0] * (n + 1)
     for i in range(2, n + 1):
-        operations[i] = float('inf')
-        for j in range(1, i):
+        dp[i] = i
+        j = 2
+        while j * j <= i:
             if i % j == 0:
-                operations[i] = min(operations[i], operations[j] + (i // j))
+                dp[i] = min(dp[i], dp[j] + i // j)
+                dp[i] = min(dp[i], dp[i // j] + j)
+            j += 1
+    return dp[n]
+
     
-    return operations[n] if operations[n] != float('inf') else 0
-
-# Example usage
-n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
