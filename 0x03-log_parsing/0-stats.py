7b28e0e4f6ced6ea76a8482902d8309a1bce4f73
mainangaruiya <003paulmaina@gmail.com>
2023-10-20 20:09:07 +0300

    script that reads stdin line by line and computes metrics:

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
 '''a script that reads stdin line by line and computes metrics'''
-
-
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
+log_format = r'(\d+\.\d+\.\d+\.\d+) - \[.+\] "
+    GET /projects/260 HTTP/1.1" (\d+) (\d+)'
+
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
+                    '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-counter = 0
+lines_processed = 0
 
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+        match = re.match(log_format, line)
+        if match:
+            ip_address, status_code, file_size = match.groups()
+            total_size += int(file_size)
+            status_codes_dict[status_code] += 1
+            lines_processed += 1
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+            # Print statistics after every 10 lines
+            if lines_processed % 10 == 0:
+                print("Total file size: File size:", total_size)
+                for code in sorted(status_codes_dict.keys()):
+                    print(f"{code}: {status_codes_dictt[code]}")
+                print()
 
-except Exception as err:
-    pass
+except KeyboardInterrupt:
+    # Handle keyboard interruption (CTRL + C)
+    print("Keyboard interruption detected. Printing final statistics.")
+    print("Total file size: File size:", total_size)
+    for code in sorted(status_codes_dict.keys()):
+        print(f"{code}: {status_codes_dict[code]}")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
 
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+# Print final statistics after processing all input
+print("Total file size: File size:", total_size)
+for code in sorted(status_codes_dict.keys()):
+    print(f"{code}: {status_codes_dict[code}")
