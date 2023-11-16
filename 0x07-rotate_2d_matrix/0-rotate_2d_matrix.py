0f926e9119a9b3129bc67bfece5fd7d4120f3fb9
mainangaruiya <003paulmaina@gmail.com>
2023-11-16 10:34:22 +0300

    Given an n x n 2D matrix, rotate it 90 degrees clockwise.

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,26 +4,33 @@
 
 
 def rotate_2d_matrix(matrix):
-	"""module 
-	"""
-	if type(matrix) != list:
-		return
-	if len(matrix) <= 0:
-		return
-	if not all(map(lamda x: type(x) == list, matrix)):
-		return
-	rows = len(matrix)
-	cols = len(matrix[0])
-	if not all(map(lambda x: len(x) == cols, matrix)):
-		return
-	c, r=0, rows -1
-	for i in range(cols * rows):
-		if i % rows == 0:
-			matrix.append([])
-		if r == -1:
-			r = rows -1
-			c += 1
-		matrix[-1].append(matrix[r][c])
-		if c == cols -1 and r >= -1:
-			matrix.pop(r)
-		r -= 1
+    """Rotate a 2D matrix clockwise
+    """
+    if not isinstance(matrix, list):
+        return
+
+    if not matrix or not all(isinstance(row, list) for row in matrix):
+        return
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    if not all(len(row) == cols for row in matrix):
+        return
+
+    c, r = 0, rows - 1
+
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+
+        if r == -1:
+            r = rows - 1
+            c += 1
+
+        matrix[-1].append(matrix[r][c])
+
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+
+        r -= 1
