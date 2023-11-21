21307a07d2caf075f2c9be47c9d5dba9a07b9af7
mainangaruiya <003paulmaina@gmail.com>
2023-11-21 10:32:04 +0300

    Prototype: def makeChange(coins, total) Return: fewest number of coins needed to meet total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+module
+"""
+def makeChange(coins, total):
+	if total <= 0:
+		return 0
+	rem = total
+	coins_count = 0
+	coin_idx = 0
+	sorted_coins = sorted(coins, reverse=True)
+	n = len(coins)
+	while rem > 0:
+		if coin_idx >= n:
+			return -1
+		if rem - sorted_coins[coin_idx] >= 0:
+			rem -= sorted_coins[coin_idx]
+			coins_count += 1
+		else:
+			coin_idx += 1
+	return coins_count
